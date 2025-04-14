ea1ce10f4d63a1401326c6b84b819fefb409d018
Olivia Stead <oliviastead04@gmail.com>
2025-04-14 09:20:24 +0100

    initdb&fb

---
+++ db/database.js
@@ -0,0 +1,21 @@
+"use strict";
+
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+let db;
+
+async function connectToDB() {
+    await client.connect();
+    db = client.db();
+}
+
+function getDB() {
+    return db;
+}
+
+export default { connectToDB, getDB };
---
+++ middleware/auth.js
@@ -0,0 +1,31 @@
+import fb from "../fb/firebase.js";
+
+async function createSessionCookie(req, res, next) {
+    const expiresIn = 1000 * 60 * 60 * 24 * 5;
+    const idToken = req.body.idToken;
+
+    try {
+        const sessionCookie = await fb.auth().createSessionCookie(idToken, { expiresIn });
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        next();
+    } catch (error) {
+        error.status = 401;
+        next(error);
+    }
+}
+
+async function allowed(req, res, next) {
+    const sessionCookie = req.cookies.session || "";
+
+    try {
+        const decoded = await fb.auth().verifySessionCookie(sessionCookie, true);
+        res.locals.uid = decoded.uid;
+        next();
+    } catch (error) {
+        error.status = 401;
+        next(error);
+    }
+}
+
+export default { createSessionCookie, allowed };
