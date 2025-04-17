bcbc4bfebfd9c33e7e7f0af5b49dc118be018c2e
Olivia Stead <oliviastead04@gmail.com>
2025-04-17 10:40:40 +0100

    index

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import db from "./db/database.js";
+import usersRouter from "./routes/users.js";
+import ordersRouter from "./routes/orders.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use("/", usersRouter);
+app.use("/orders", ordersRouter);
+
+db.connectToDB().then(() =>
+{
+    app.listen(PORT, () =>
+    {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}).catch((err) =>
+{
+    console.error("Failed to connect to DB", err);
+});
