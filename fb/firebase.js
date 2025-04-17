180bbc0851554c5bba2a0b20391926a63d423f7c
Olivia Stead <oliviastead04@gmail.com>
2025-04-17 12:01:22 +0100

    filecorrection

--- fb/firebase.js
+++ fb/firebase.js
@@ -7,7 +7,7 @@ const require = createRequire(import.meta.url);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const data = require(path.join(__dirname, "../key.json"));
+const data = require(path.join(__dirname, "key.json"));
 
 admin.initializeApp({
     credential: admin.credential.cert(data)
