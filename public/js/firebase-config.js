e8ff992e16005ee2277f18de12b10015efdf3130
Olivia Stead <oliviastead04@gmail.com>
2025-04-20 19:24:54 +0100

    firebasemoved

---
+++ public/js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCLpPObnjknqhFCp7sylBAiEkvbuJmRvP4",
+    authDomain: "themeparkapp-b896f.firebaseapp.com",
+    projectId: "themeparkapp-b896f",
+    storageBucket: "themeparkapp-b896f.firebasestorage.app",
+    messagingSenderId: "924840648129",
+    appId: "1:924840648129:web:e327454c0c1e904b362340"
+    };
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();    
