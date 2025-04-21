c93c951c13b16cd173a6066d09e272c238a8be4c
Olivia Stead <oliviastead04@gmail.com>
2025-04-21 17:44:28 +0100

    debugging

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,10 +3,12 @@ import { admin as fb } from "../fb/firebase.js";
 async function createSessionCookie(req, res, next) {
     const expiresIn = 1000 * 60 * 60 * 24 * 5;
     const idToken = req.body.idToken;
-    console.log("received ID token backend", idToken)
 
     try {
         const sessionCookie = await fb.auth().createSessionCookie(idToken, { expiresIn });
+
+        console.log("Session Cookie Created:", sessionCookie);
+
         const options = { maxAge: expiresIn, httpOnly: true };
         res.cookie("session", sessionCookie, options);
         next();
@@ -18,11 +20,13 @@ async function createSessionCookie(req, res, next) {
 
 async function allowed(req, res, next) {
     const sessionCookie = req.cookies.session || "";
+    console.log("Session Cookie Retrieved:", sessionCookie);
 
     try {
         res.locals.user = (await fb.auth().verifySessionCookie(sessionCookie, true)).uid;
         next();
     } catch (error) {
+        console.error("Session verification failed:", error);
         error.status = 401;
         next(error);
     }
