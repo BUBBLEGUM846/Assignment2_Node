13a4c494a9d3c5c43b816ca90f087bef85212cf8
Olivia Stead <oliviastead04@gmail.com>
2025-05-11 20:00:12 +0100

    test remove

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,7 @@ router.get("/sign-in", (req, res) =>
     res.render("sign-in");
 });
 
+//sign in creates a session cookie
 router.post("/sign-in", auth.createSessionCookie, (req, res) =>
 {
     res.status(200).end();
@@ -31,19 +32,21 @@ router.post("/sign-up", async (req, res, next) => {
             password,
         });
 
+        //insert user in my database
         await getDB().collection("users").insertOne({
             uid: userRecord.uid,
             email: userRecord.email,
             createdOn: new Date()
         });
 
+        //doesnt auto aign-in
         res.redirect("/sign-in");      
     } catch (error) {
         console.error("Error creating user:", error);
         res.status(500).send("Error registering user");
     }
 })
-
+/* 
 router.get("/", auth.allowed, async (req, res, next) =>
 {
     try {
@@ -53,7 +56,7 @@ router.get("/", auth.allowed, async (req, res, next) =>
     } catch(error) {
         next(error);
     }
-});
+}); */
 
 
 router.get("/logout", (req, res) => {
